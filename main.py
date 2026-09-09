875701619dd964de979481e39ab73f5635a8ec50
nkyadav725024839 <nkyadav725024839@gmail.com>
2026-09-09 09:49:09 +0530

    Add handlers for ask tutor and tutor explain
    Added handlers for tutor-related callbacks.

--- main.py
+++ main.py
@@ -4290,6 +4290,9 @@ async def main():
         app.add_handler(CallbackQueryHandler(view_my_quizzes, pattern="^btn_viewquizzes$"))
         app.add_handler(CallbackQueryHandler(handle_back_main, pattern="^back_main$"))
         app.add_handler(CallbackQueryHandler(handle_view_quiz_callback, pattern="^viewq_"))
+        # leaderboard के बाद add करें (लगभग line 3200):
+        app.add_handler(CallbackQueryHandler(handle_ask_tutor, pattern="^asktutor_"))
+        app.add_handler(CallbackQueryHandler(handle_tutor_more, pattern="^tutor_explain_"))
         
         app.add_handler(CallbackQueryHandler(handle_ready_click, pattern="^ready_"))
         app.add_handler(CallbackQueryHandler(handle_start_private, pattern="^startprivate_"))
